a098fff0c58620ca8f9df039b5a65488aefac8c3
Emre Yilmaz <mail@emreyilmaz.me>
2014-08-08 12:21:55 +0300

    add static/media settings.

--- lkdcourses/base/settings.py
+++ lkdcourses/base/settings.py
@@ -82,3 +82,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = '/media/'
