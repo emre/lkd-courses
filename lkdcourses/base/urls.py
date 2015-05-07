c65ebf471d5272c937c64e262601233d1e4fcd9e
Berker Peksag <berker.peksag@gmail.com>
2015-05-07 15:23:48 +0300

    Add password reset form.
    Closes #11.

--- lkdcourses/base/urls.py
+++ lkdcourses/base/urls.py
@@ -6,6 +6,15 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
+    url(r'^resetpassword/done/$', 'django.contrib.auth.views.password_reset_done',
+        {'template_name': 'registration/reset_done.html'}, name='password_reset_done'),
+    url(r'^resetpassword/complete/$', 'django.contrib.auth.views.password_reset_complete',
+        {'template_name': 'registration/reset_complete.html'}, name='password_reset_complete'),
+    url(r'^resetpassword/$', 'django.contrib.auth.views.password_reset',
+        {'template_name': 'registration/reset.html'}, name='password_reset'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        {'template_name': 'registration/reset_form.html'}, name='password_reset_confirm'),
     url(r'^', include('courses.urls', namespace='courses')),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- lkdcourses/manage.py
+++ lkdcourses/manage.py
@@ -3,7 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "base.settings")
+    if os.path.exists('base/local_settings.py'):
+        settings = 'base.local_settings'
+    else:
+        settings = 'base.settings'
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
 
     from django.core.management import execute_from_command_line
 
